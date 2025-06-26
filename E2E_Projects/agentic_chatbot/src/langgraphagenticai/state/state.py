41a6e2d117bcb76fef494922e4c76fffb0660231
dhineshbabu <dhineshbabu89@gmail.com>
2025-06-25 19:50:06 -0700

    added simple E2E chatbot project

---
+++ E2E_Projects/agentic_chatbot/src/langgraphagenticai/state/state.py
@@ -0,0 +1,7 @@
+from typing_extensions import TypedDict,List
+from langgraph.graph.message import add_messages
+from typing import Annotated
+
+class State(TypedDict):
+    """ Represents the structure of the state used in the agent graph"""
+    messages: Annotated[List[str], add_messages]
